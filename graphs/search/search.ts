a87cd13eaf81812ef0e36c39f9d533f1dcb8185c
Brandie Lucano <blucano.dev@gmail.com>
2024-02-29 17:06:11 -0800

    search iDfs works and tests passing.

--- graphs/search/search.ts
+++ graphs/search/search.ts
@@ -1,4 +1,5 @@
 import { GNodeStr } from "../graph/graph";
+import { Stack } from "../common/stack";
 
 /** Return array of nodes, in DFS order (recursive version)  */
 
@@ -22,7 +23,28 @@ function rDfs(
 /** Return array of nodes, in DFS order (iterative version)  */
 
 function iDfs(start: GNodeStr): string[] {
-  return ["todo"];
+
+  const stack = new Stack<GNodeStr>([start]);
+  const result : string[] = [];
+  const visited = new Set<GNodeStr>();
+
+  visited.add(start);
+
+  while(!stack.isEmpty()){
+    let currNode = stack.pop();
+    result.push(currNode.value);
+
+    for(const adj of currNode.adjacent){
+      if(!visited.has(adj)){
+        visited.add(adj);
+        stack.push(adj);
+      }
+
+    }
+
+  }
+
+  return result;
 }
 
 /** Return array of nodes, in BFS order (recursive version)  */
