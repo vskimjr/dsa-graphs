0e2d671258365d2a555e57f27b8be81a510f143a
Brandie Lucano <blucano.dev@gmail.com>
2024-02-29 16:06:36 -0800

    graph working and tests pass. working on fDfs

--- graphs/graph/graph.ts
+++ graphs/graph/graph.ts
@@ -21,22 +21,41 @@ class GraphStr {
 
   /** Add node to graph. */
   addNode(node: GNodeStr): void {
+    this.nodes.add(node);
   }
 
   /** Add array of nodes to graph. */
   addNodes(nodeArray: GNodeStr[]): void {
+    for(const n of nodeArray){
+      this.addNode(n);
+    }
   }
 
   /** Add edge between v1 and v2. */
   addEdge(v1: GNodeStr, v2: GNodeStr): void {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
   }
 
+  /**Add edges for array of node pairs */
+  addEdges(edgeArray: GNodeStr[][]): void{
+    for(const e of edgeArray){
+      this.addEdge(e[0], e[1]);
+    }
+  }
   /** Remove edge between v1 and v2. */
   removeEdge(v1: GNodeStr, v2: GNodeStr): void {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
   }
 
   /** Remove node from graph. */
   removeNode(node: GNodeStr): void {
+    for(const n of node.adjacent){
+      n.adjacent.delete(node);
+      node.adjacent.delete(n);
+    }
+    this.nodes.delete(node);
   }
 }
 
